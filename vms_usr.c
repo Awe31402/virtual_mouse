10fc1533185e8786f3ccce0a7736df741064ab10
edward.wang <edward.wang@insydesw.com>
2015-04-14 16:11:07 +0800

    Added envent experiment user space.

--- vms_usr.c
+++ vms_usr.c
@@ -23,6 +23,7 @@ int main(int argc, char *argv[])
 		if (x%2) x = -x; 
 		if (y%2) y = -y;
 
+		printf("wirte to attribute file : %d %d %d\n", x, y, 0);
 		sprintf(buffer, "%d %d %d", x, y, 0);
 		write(sim_fd, buffer, strlen(buffer));
 		fsync(sim_fd);
